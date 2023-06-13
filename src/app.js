55a7e0ac25a0dbd7c05adfc3179e7cc3d2488801
Kieu Anh (Katie) Pham <phamkieuanh33@gmail.com>
2023-06-13 19:16:14 +1000

    Show current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let weatherDescriptionElement = document.querySelector(
+    "#weather-description"
+  );
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
 
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  weatherDescriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "cc839d6b2e6eab8ff6b88460afc66315";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
