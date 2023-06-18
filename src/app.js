03793ee3fc5e8115e3e4c03f228d8ac0db29f00e
Kieu Anh (Katie) Pham <phamkieuanh33@gmail.com>
2023-06-19 00:00:27 +1000

    Display forecast data using API

--- src/app.js
+++ src/app.js
@@ -19,7 +19,6 @@ function formatDate(now) {
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday",
   ];
   let day = days[now.getDay()];
 
@@ -118,30 +117,47 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Sydney");
 
+function formatDay(timestamps) {
+  let date = new Date(timestamps * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
 
-  let days = ["Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
             <div class="col-2">
-              <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forecast-date">${formatDay(
+                forecastDay.dt
+              )}</div>
               <img
-                src="https://openweathermap.org/img/wn/01d.png"
+                src="https://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 alt=""
                 width="42"
               />
               <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temperature-max"> 18°</span>
-                <span class="weather-forecast-temperature-min"> 12°</span>
+                <span class="weather-forecast-temperature-max"> ${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="weather-forecast-temperature-min"> ${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
               </div>
             </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
