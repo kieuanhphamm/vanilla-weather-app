e1b58b8287ca3f720c93ca03f14d40e96c03bb91
Kieu Anh (Katie) Pham <phamkieuanh33@gmail.com>
2023-06-18 23:24:54 +1000

    Add daily forecast API

--- src/app.js
+++ src/app.js
@@ -67,6 +67,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -116,7 +118,8 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Sydney");
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -145,3 +148,11 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 displayForecast();
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "017d56650cd168d68067850318775d43";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
