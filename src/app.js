ec9dcb84c138509bfd69090018f30ec5c9f56108
Kieu Anh (Katie) Pham <phamkieuanh33@gmail.com>
2023-06-23 00:36:52 +1000

    Update main icon size and update CSS for main elements

--- src/app.js
+++ src/app.js
@@ -63,7 +63,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(current);
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
