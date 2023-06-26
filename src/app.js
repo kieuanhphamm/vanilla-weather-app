368063006ae43a829fc426a844a3bd71db792b39
Kieu Anh (Katie) Pham <phamkieuanh33@gmail.com>
2023-06-26 20:03:57 +1000

    Add 1 more day to the forecast

--- src/app.js
+++ src/app.js
@@ -117,11 +117,11 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 7) {
       forecastHTML =
         forecastHTML +
         `
-            <div class="col-2">
+            <div class="col">
               <div class="weather-forecast-date">${formatDay(
                 forecastDay.dt
               )}</div>
